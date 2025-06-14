a3685c77d441000acb10e5941275faec4a61818d
OdenEater <katta0118@gmail.com>
2025-06-15 01:53:59 +0900

    初回コミット

---
+++ wedding-board/src/main/java/com/example/controller/HelloServlet.java
@@ -0,0 +1,36 @@
+package com.example.controller;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public HelloServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		String name = request.getParameter("name");
+		System.out.println(name);
+		request.getRequestDispatcher("/next.html").forward(request, response);
+	}
+
+}
